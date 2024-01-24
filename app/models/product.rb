a2cb35a1fb56af7259cac8419d9dd91edf712e2c
Diego Gomez <diego@example.com>
2024-01-24 14:56:13 +0000

    update sti and add scope to get the top 5 products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,9 @@
 class Product < ActiveRecord::Base
+  self.inheritance_column = 'something_else'
+
   has_and_belongs_to_many  :categories, join_table: :product_categories
   has_many :purchases
   has_many :photos, as: :photoable
-  #belongs_to :admin, class_name: "Admin", foreign_key: "admin_id"
+
+  scope :top, -> { order('price DESC').limit(5) }
 end
