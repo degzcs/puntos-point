eb04a749ac05662cb369a8167a04f8201df6723d
Diego Gomez <diego@example.com>
2024-01-27 06:09:11 +0000

    add query to get top products by category

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,45 @@
 class Product < ActiveRecord::Base
-  self.inheritance_column = 'something_else'
+  self.inheritance_column = 'sty_type'
 
   has_and_belongs_to_many  :categories, join_table: :product_categories
+  has_many :product_categories
   has_many :purchases
   has_many :photos, as: :photoable
 
-  scope :top, -> { order('price DESC').limit(5) }
+  validates :name, presence: true, uniqueness: true
+
+  #
+  # Class methods
+  #
+
+  # This method is used to find the number of purchases for each product in each category.
+  # It is used in the next method (categories_and_max_purchase) to find the maximum number of purchases for each category.
+  # It is also used in the method after that (top_products_by_category) to find the top product for each category.
+  def self.purchase_count_by_category
+    Purchase.select("purchases.product_id, product_categories.category_id, COUNT(purchases.id) as purchase_count")
+      .joins("INNER JOIN product_categories ON purchases.product_id = product_categories.product_id")
+      .group("purchases.product_id, product_categories.category_id")
+  end
+
+  # This method is used to find the maximum number of purchases for each category.
+  # It is used in the next method to find the top product for each category.
+  def self.categories_and_max_purchases
+    ProductCategory.select("product_categories.category_id, MAX(prod_and_category.purchase_count) as max_purchase_count")
+      .joins("INNER JOIN (#{purchase_count_by_category.to_sql}) AS prod_and_category ON product_categories.product_id = prod_and_category.product_id")
+      .group("product_categories.category_id")
+  end
+
+  # This method is used to find the top product for each category.
+  # It is used in the controller to display the top product for each category.
+  def self.top_products_by_category
+    query = Product.select("products.*")
+      .joins("INNER JOIN (#{purchase_count_by_category.to_sql}) AS prod_and_category ON products.id = prod_and_category.product_id")
+      .where("(prod_and_category.category_id, prod_and_category.purchase_count) IN (#{categories_and_max_purchases.to_sql})")
+
+    Product.find_by_sql(query.to_sql)
+  end
+
+  #
+  # Instance methods
+  #
 end
---
+++ spec/models/products_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe Product, :type => :model do
+  context '#scopes' do
+    let!(:product1) { create(:product, :with_category) }
+    let(:category1) { product1.categories.first }
+    let!(:product2) { create(:product, categories: [category1]) }
+
+    let!(:product3) { create(:product, :with_category) }
+    let(:category2) { product3.categories.first }
+    let!(:product4) { create(:product, categories: [category2]) }
+
+    let!(:purchase1) { create_list(:purchase, 3, product: product1) }
+    let!(:purchase2) { create_list(:purchase, 5, product: product2) }
+    let!(:purchase3) { create_list(:purchase, 2, product: product4) }
+
+    it 'retrieves the top products by each category' do
+      expect(Product.top_products_by_category).to include(product2, product4)
+    end
+  end
+end
